81de2a5ff653f4401b5bd725d87180efcb32fcfd
Adam & Jason <adam+jason@appacademy.io>
2013-02-01 10:57:33 -0800

    fixed email and password validation bugs.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   validates :name, :uniqueness => true, :presence => true
   validates :email, :uniqueness => true, :presence => true
-  validates_format_of :email, :with => %r\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :on => :create
-  validates :password, :length => { :minimum 5, 
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :on => :create
+  validates :password, :length => { minimum: 5,
             :too_short => "must have at least 5 letters"}
   has_secure_password
 
